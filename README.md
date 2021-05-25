27be00537b699a0bc18b39171a0e9af68a9549cb
khanhphamqn <khanhphamqn@gmail.com>
2021-05-25 20:41:42 +0700

    first code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# strapi
-API using strap
+# Strapi application
+
+A quick description of your strapi application
---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6efcb18110b740e4834d764a0c96b06f'),
+    },
+  },
+});
