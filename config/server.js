1ad19d3aaeabf3364f887f0dfc017699b10e03c2
khanhphamqn <khanhphamqn@gmail.com>
2021-05-27 09:42:20 +0700

    update code

--- config/server.js
+++ config/server.js
@@ -1,11 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://strapi-sqllite-test.netlify.app", 
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6efcb18110b740e4834d764a0c96b06f'),
-    },
-    serveAdminPanel: false
+    }
   },
 });
